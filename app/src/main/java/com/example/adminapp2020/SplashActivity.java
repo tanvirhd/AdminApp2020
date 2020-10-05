316aa7e86230ce9c0298bfaf6a53cdada3790f28
tanvirhd <tanvirahc15@gmail.com>
2020-10-05 21:57:40 +0600

    UserChecking on Splash Screen Done.

--- app/src/main/java/com/example/adminapp2020/SplashActivity.java
+++ app/src/main/java/com/example/adminapp2020/SplashActivity.java
@@ -1,12 +1,19 @@
 package com.example.adminapp2020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.adminapp2020.authentication.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -14,11 +21,26 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if(FirebaseAuth.getInstance().getCurrentUser()==null){
-            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class)); finish();
         }else {
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        }
+            DatabaseReference ref= FirebaseDatabase.getInstance().getReference("admin").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class)); finish();
+                    }else{
+                        startActivity(new Intent(SplashActivity.this, LoginActivity.class)); finish();
+                    }
 
-        finish();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
     }
 }
